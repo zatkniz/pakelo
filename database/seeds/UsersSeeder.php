906ad3de1e1463684db6cfcd322af3894d1d84ab
Stelios Kitziris <skitziris@draxis.gr>
2019-12-05 17:00:25 +0200

    add users ssed

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -11,13 +11,31 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-        \App\User::create([
-            'name' => 'Στέλιος Κιτζίρης',
-            'username' => 'stkitziris',
-            'email' => 'stkitziris@gmail.com',
-            'password' => 'test1234',
-            'active' => true,
-            'user_role_id' => 1,
-        ]);
+        \App\User::create(
+            [
+                'name' => 'Στέλιος Κιτζίρης',
+                'username' => 'stkitziris',
+                'email' => 'stkitziris@gmail.com',
+                'password' => 'test1234',
+                'active' => true,
+                'user_role_id' => 1,
+            ],
+            [
+                'name' => 'Στάυρος Σωτηριάδης',
+                'username' => 'stauros',
+                'email' => 'stauros_swt@hotmail.com',
+                'password' => '12345',
+                'active' => true,
+                'user_role_id' => 1,
+            ],
+            [
+                'name' => 'Παρασκευάς Παρασκευάς',
+                'username' => 'paraskeuas',
+                'email' => 'paraskeuas@paraskeuas.com',
+                'password' => '12345',
+                'active' => true,
+                'user_role_id' => 1,
+            ]
+        );
     }
 }
